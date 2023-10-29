cfa76675b2a0e7454456eaf6608c559110647d65
darkclyan <gustavober59@gmail.com>
2023-10-29 20:43:57 -0300

    chore: Remove package-lock.json files

--- atm-api/build/routes/atm.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const atm_1 = require("../controllers/atm");
-const router = (0, express_1.Router)();
-router.put('/withdraw', atm_1.withdraw);
-exports.default = router;
