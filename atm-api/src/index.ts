2af45ec0b4f7f8ab098f5a02dd36b46427d7ded2
Saulo Coimbra <saulo.coimbra1@gmail.com>
2023-12-22 15:46:55 -0300

    I created the globalstyles folder and put sources all the same

--- atm-api/src/index.ts
+++ atm-api/src/index.ts
@@ -1,23 +1,23 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-import express from 'express';
-import mongoose, { Error } from 'mongoose';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import compression from 'compression';
-import statusRoutes from './routes/status';
-import accountsRoutes from './routes/accounts';
-import atmRoutes from './routes/atm';
-import customersRoutes from './routes/customers';
-import authenticationRoutes from './routes/authentication';
+import express from "express";
+import mongoose, { Error } from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import statusRoutes from "./routes/status";
+import accountsRoutes from "./routes/accounts";
+import atmRoutes from "./routes/atm";
+import customersRoutes from "./routes/customers";
+import authenticationRoutes from "./routes/authentication";
 
 const app = express();
-const uri = process.env.MONGODB_URI ?? 'mongodb://localhost:27017/cluster0';
+const uri = process.env.MONGODB_URI ?? "mongodb://localhost:27017/cluster0";
 
 mongoose
   .connect(uri)
-  .then(() => console.log('MongoDB connected'))
+  .then(() => console.log("MongoDB connected"))
   .catch((error: Error) => console.log(error));
 
 app.use(
@@ -30,11 +30,11 @@ app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-app.use('/api', statusRoutes);
-app.use('/accounts', accountsRoutes);
-app.use('/atm', atmRoutes);
-app.use('/customers', customersRoutes);
-app.use('/authentication', authenticationRoutes);
+app.use("/api", statusRoutes);
+app.use("/accounts", accountsRoutes);
+app.use("/atm", atmRoutes);
+app.use("/customers", customersRoutes);
+app.use("/authentication", authenticationRoutes);
 
 const PORT = process.env.PORT ?? 3001;
 
